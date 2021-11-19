32831de99edf08f753144795d8ba0de4c776158c
TobyCotton <94650770+TobyCotton@users.noreply.github.com>
2021-11-19 01:28:16 +0000

    Update .cpp

--- SnakeGame/.cpp
+++ SnakeGame/.cpp
@@ -2,9 +2,9 @@
 
 Button::Button(sf::Font font, std::string message, int size, sf::Vector2f position, sf::Color colour)
 {
-	sf::Text Title;
-	font.loadFromFile("PressStart2p-vaV7.ttf");
-	Title.setFont(font);
+	sf::Text Title; 
+	font.loadFromFile("PressStart2p-vaV7.ttf"); // loading text font
+	Title.setFont(font); // setting text value
 	Title.setString(message);
 	Title.setCharacterSize(size);
 	Title.setPosition(position);
